76837699831c4a6c01df086ca19b15ba9d86f6ab
Junya Sato <sato@metaps-payment.com>
2024-03-17 13:20:30 +0900

    調整

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,36 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import styles from "../styles/Home.module.css"
 import Layout from "../components/Layout";
 
 import Link from "next/link";
-
-const inter = Inter({ subsets: ["latin"] });
+import utilStyle from "../styles/utils.module.css"
 
 export default function Home() {
-  return <Layout></Layout>
+  return (
+    <Layout>
+      <section className={utilStyle.headingMd}>
+        <p>
+          私はフルスタックエンジニアです。/Udemy講師をしています。/好きな言語はJavaScriptです
+        </p>
+      </section>
+      <section>
+        <h2>📝エンジニアのブログ</h2>
+        <div className={styles.grid}>
+          <article>
+            <Link href="/">
+              <img src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link href="/" className={utilStyle.boldText}>
+                SSGとSSRの使い分けの場面はいつなのか？
+            </Link>
+            <br />
+            <small className={utilStyle.lightText}>
+              Feburary 23, 2020
+            </small>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  );
 }
